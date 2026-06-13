70ff466e002d42e88a301e593bb04c074ef9be56
Javier Vivo Samaniego <javier.vivo1@estudiant.uib.cat>
2026-06-13 21:13:35 +0200

    Update Day30Milestone.tsx

--- src/features/stats/milestones/Day30Milestone.tsx
+++ src/features/stats/milestones/Day30Milestone.tsx
@@ -152,7 +152,7 @@ export const Day30Milestone: StreakMilestone = {
         />
 
         {/* Flame sitting on the book like a reading lamp */}
-        <g transform="translate(38, 60)">
+        <g transform="translate(39, 60)">
           <motion.path
             d="M8.5 14.5A2.5 2.5 0 0 0 11 12c0-1.38-.5-2-1-3-1.072-2.143-.224-4.054 2-6 .5 2.5 2 4.9 4 6.5 2 1.6 3 3.5 3 5.5a7 7 0 1 1-14 0c0-1.153.433-2.294 1-3a2.5 2.5 0 0 0 2.5 2.5z"
             fill="#f97316"
@@ -355,7 +355,7 @@ export const Day30Milestone: StreakMilestone = {
 
     // Flame on book
     ctx.save();
-    ctx.translate(ox + 38, oy + 60);
+    ctx.translate(ox + 39, oy + 60);
     ctx.scale(0.85, 0.85);
     ctx.shadowColor = "rgba(249, 115, 22, 0.7)";
     ctx.shadowBlur = 10;
